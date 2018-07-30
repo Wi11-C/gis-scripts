b90e513adde6b917fd5f6497f045d747f64ebd2c
Will <Wi11-C@users.noreply.github.com>
2018-07-30 08:10:42 -0400

    Update setup.bat

--- network/setup.bat
+++ network/setup.bat
@@ -1,7 +1,9 @@
 IF "%PYQGIS_STARTUP%"=="" (
-    SET PYQGIS_STARTUP="%APPDATA%\QGIS\QGIS3\profiles\default\python\startup.py"
+    SETX PYQGIS_STARTUP "%APPDATA%\QGIS\QGIS3\profiles\default\python\startup.py"
 ) ELSE (
     ECHO env var already set
 )
 
-xcopy /y %~dp0startup.py "%APPDATA%\QGIS\QGIS3\profiles\default\python"
+xcopy /Y "%~dp0startup.py" "%APPDATA%\QGIS\QGIS3\profiles\default\python\startup.py"
+
+timeout /t -1
