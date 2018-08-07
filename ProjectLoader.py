cd39c807927be132643c8a5757ab5279050e553a
Will Carey <substarte@gmail.com>
2018-08-06 22:36:02 -0400

    changes

--- ProjectLoader.py
+++ ProjectLoader.py
@@ -10,10 +10,15 @@ def LoadProjects(filepath):
 class ProjectName:
     self.RawName = ''
     self.Number = ''
+    self.corridor = ''
+    self.start = ''
+    self.end = ''
     self.is_intersection = False
     def __init__(self, name, number):
         self.RawName = name
         self.Number = number
 
     def split_name(self):
-        
+        self.corridor = self.RawName.split('.',1)[0]
+        if self.corridor == '':
+            self.corridor = self.RawName.split(';', 1)[0]
